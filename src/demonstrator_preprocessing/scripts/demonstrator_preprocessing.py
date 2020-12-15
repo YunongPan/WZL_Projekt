22724cba9d1366567070f529e83224ed1c0f5786
yunong <yunong.pan@rwth-aachen.de>
2020-12-15 10:22:05 +0100

    start the rear lidar and add ur10 to model

--- src/demonstrator_preprocessing/scripts/demonstrator_preprocessing.py
+++ src/demonstrator_preprocessing/scripts/demonstrator_preprocessing.py
@@ -6,11 +6,16 @@ import time
 import math
 import tf
 import geometry_msgs.msg
+import message_filters
 from sensor_msgs.msg import LaserScan
 
 class Demonstrator():
   def __init__(self):
-    self.rosbot_sub = rospy.Subscriber("/laserscan/front", LaserScan, self.scan_callback)
+    self.laser_front_sub = message_filters.Subscriber("laserscan/front", LaserScan)
+    self.laser_rear_sub = message_filters.Subscriber("laserscan/rear", LaserScan)
+    self.ts = message_filters.ApproximateTimeSynchronizer([self.laser_front_sub, self.laser_rear_sub], queue_size = 10, slop=0.5)
+    self.ts.registerCallback(self.scan_callback)
+
     self.listener = tf.TransformListener()
 
     self.angle_min = -1.570796
@@ -18,7 +23,8 @@ class Demonstrator():
     self.sample = 720
     self.angle_increment = (self.angle_max - self.angle_min) / self.sample   
  
-    self.nearest_point_range = 0.0
+    self.nearest_point_range_front = 0.0
+    self.nearest_point_range_rear = 0.0
     self.nearest_point_index = 0.0
     self.nearest_point_angle = 0.0
     self.nearest_point_x = 0.0
@@ -31,24 +37,39 @@ class Demonstrator():
     self.rate = rospy.Rate(1)
     rospy.on_shutdown(self.shutdownhook)
 	
-  def scan_callback(self, msg):
-    self.nearest_point_range = min(msg.ranges)
-    self.nearest_point_index = msg.ranges.index(min(msg.ranges))    
-    self.nearest_point_angle = self.nearest_point_index * self.angle_increment + self.angle_min
-		
+  def scan_callback(self, laser_front, laser_rear):
+    self.nearest_point_range_front = min(laser_front.ranges)
+    self.nearest_point_range_rear = min(laser_rear.ranges)
+    if self.nearest_point_range_front < self.nearest_point_range_rear:
+      self.nearest_point_index = laser_front.ranges.index(min(laser_front.ranges))    
+      self.nearest_point_angle = self.nearest_point_index * self.angle_increment + self.angle_min
+      self.nearest_point_x = self.nearest_point_range_front * math.cos(self.nearest_point_angle)
+      self.nearest_point_y = self.nearest_point_range_front * math.sin(self.nearest_point_angle)
+    else:
+      self.nearest_point_index = laser_rear.ranges.index(min(laser_rear.ranges))    
+      self.nearest_point_angle = self.nearest_point_index * self.angle_increment + self.angle_min
+      self.nearest_point_x = self.nearest_point_range_rear * math.cos(self.nearest_point_angle)
+      self.nearest_point_y = self.nearest_point_range_rear * math.sin(self.nearest_point_angle)
 
   def read_laser(self):
     while not self.ctrl_c:
 
-      self.nearest_point_x = self.nearest_point_range * math.cos(self.nearest_point_angle)
-      self.nearest_point_y = self.nearest_point_range * math.sin(self.nearest_point_angle)
+
       try:
+        if self.nearest_point_range_front < self.nearest_point_range_front:
           (trans,rot) = self.listener.lookupTransform('/chassis', '/hokuyo_front', rospy.Time(0))
+          self.nearest_point_to_chassis_x = self.nearest_point_x + trans[0]
+          self.nearest_point_to_chassis_y = self.nearest_point_y + trans[1]
+          self.nearest_point_to_chassis = math.sqrt(self.nearest_point_to_chassis_x ** 2 + self.nearest_point_to_chassis_y ** 2)
+
+        else:
+          (trans,rot) = self.listener.lookupTransform('/chassis', '/hokuyo_rear', rospy.Time(0))
+
       except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
           continue
    
-      self.nearest_point_to_chassis_x = self.nearest_point_x + trans[0]
-      self.nearest_point_to_chassis_y = self.nearest_point_y + trans[1]
+      self.nearest_point_to_chassis_x = self.nearest_point_x - trans[0]
+      self.nearest_point_to_chassis_y = self.nearest_point_y - trans[1]
       self.nearest_point_to_chassis = math.sqrt(self.nearest_point_to_chassis_x ** 2 + self.nearest_point_to_chassis_y ** 2)
       
       if self.nearest_point_to_chassis<5 and self.nearest_point_to_chassis>=2:
@@ -65,7 +86,7 @@ class Demonstrator():
     self.ctrl_c=True
 
 if __name__=='__main__':
-  rospy.init_node('demonstrator_preprocessing', anonymous=True)
+  rospy.init_node('demonstrator_preprocessing')
   demonstrator_object=Demonstrator()
 
   try:
