9014da0c78912f54d16b9281ce7f89543110edbe
yunong <yunong.pan@rwth-aachen.de>
2021-01-25 22:49:54 +0100

    update environmental_detection

--- src/demonstrator_preprocessing/scripts/environmental_detection_rear.py
+++ src/demonstrator_preprocessing/scripts/environmental_detection_rear.py
@@ -19,51 +19,115 @@ angle_increment = (angle_max - angle_min) / sample
 
 
 def getPosition():
+  i = 0
   j = 0
+
   listener = tf.TransformListener()
   
   position_x = list(range(non_inf_num))
   position_y = list(range(non_inf_num))
   
   
-  for i in range(len(msg.ranges)):
+  while i < len(msg.ranges):     
+    if i < len(msg.ranges)-2:  
+
+      if not math.isinf(msg.ranges[i]):
+        point_range_0 = msg.ranges[i]
+        point_range_1 = msg.ranges[i + 1]
+        point_range_2 = msg.ranges[i + 2]
     
     
-    if not math.isinf(msg.ranges[i]):
+        if abs(point_range_0 - point_range_1) < 0.03 and abs(point_range_0 - point_range_2) < 0.03:
       
-      point_range = msg.ranges[i]
-      point_angle = i * angle_increment + angle_min
-      point_x = -(point_range * math.cos(point_angle))
-      point_y = -(point_range * math.sin(point_angle))
-      count = 0
-      while not count == 1:
-        try:
-          (trans,rot) = listener.lookupTransform('/world', '/hokuyo_rear', rospy.Time(0))
-          point_x_world = point_x + trans[0]
-          point_y_world = point_y + trans[1]
+          point_range = msg.ranges[i]
+          point_angle = i * angle_increment + angle_min
+          point_x = -(point_range * math.cos(point_angle))
+          point_y = -(point_range * math.sin(point_angle))
+          count = 0
+          while not count == 1:
+            try:
+              (trans,rot) = listener.lookupTransform('/world', '/hokuyo_rear', rospy.Time(0))
+              point_x_world = point_x + trans[0]
+              point_y_world = point_y + trans[1]
           
-          count = 1
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-          continue
+              count = 1
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+              continue
 
 
-      position_x[j] = point_x_world
-      position_y[j] = point_y_world
+          position_x[j] = point_x_world
+          position_y[j] = point_y_world
       
-      j += 1
+          j += 1
+          i += 3
+        else:
+          point_range = msg.ranges[i]
+          point_angle = i * angle_increment + angle_min
+          point_x = -(point_range * math.cos(point_angle))
+          point_y = -(point_range * math.sin(point_angle))
+          count = 0
+        
+          while not count == 1:
+            try:
+              (trans,rot) = listener.lookupTransform('/world', '/hokuyo_front', rospy.Time(0))
+              point_x_world = point_x - trans[0]
+              point_y_world = point_y - trans[1]
+          
+              count = 1
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+              continue
+
+
+          position_x[j] = point_x_world
+          position_y[j] = point_y_world
+      
+          j += 1
+          i += 1
 
   
+      else:
+        i += 1
+        continue
+
+    else:
+      if not math.isinf(msg.ranges[i]):
+        point_range = msg.ranges[i]
+        point_angle = i * angle_increment + angle_min
+        point_x = point_range * math.cos(point_angle)
+        point_y = point_range * math.sin(point_angle)
+        count = 0
+        
+        while not count == 1:
+          try:
+            (trans,rot) = listener.lookupTransform('/world', '/hokuyo_front', rospy.Time(0))
+            point_x_world = point_x + trans[0]
+            point_y_world = point_y + trans[1]
+          
+            count = 1
+          except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+
+
+        position_x[j] = point_x_world
+        position_y[j] = point_y_world
+      
+        j += 1
+        i += 1
+      else:
+        i += 1
+        continue
+  
+  position_x = [x for x in position_x if not isinstance(x, int)]
+  position_y = [x for x in position_y if not isinstance(x, int)]
   return position_x, position_y
 
 def setParam():
   global non_inf_num
   non_inf_num = len(msg.ranges) - msg.ranges.count(float("inf"))
-  ScanFilterChain=list(range(non_inf_num /1))
-
   p_x, p_y = getPosition()
-
+  ScanFilterChain=list(range(len(p_x)))
   
-  for ind in range(non_inf_num /1):
+  for ind in range(len(p_x)):
     min_x = p_x[ind * 1] - 0.1
     min_y = p_y[ind * 1] - 0.1
     min_z = -5
